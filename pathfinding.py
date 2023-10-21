b12f4e9db82615cc027780a92b305a272dec586e
Emma McKeown <diamond_rain@hotmail.co.uk>
2023-10-21 19:17:42 +0200

    pathfinder

--- pathfinding.py
+++ pathfinding.py
@@ -1,3 +1,26 @@
 import pygame
 import math
 from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH)) # screen
+pygame.display.set_caption("Path Finder") # screen's name
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+
+class Spot:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
