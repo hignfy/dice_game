febee90cee64773b1aea693be62ccb58f02248ae
Emma McKeown <diamond_rain@hotmail.co.uk>
2023-10-26 14:16:12 +0200

    a-star-done

--- pathfinding.py
+++ pathfinding.py
@@ -107,7 +107,6 @@ def reconstruct_path(came_from, current, draw):
         current.make_path()
         draw()
 
-
 def algorithm(draw, grid, start, end):
     count = 0
     # get smallest element
